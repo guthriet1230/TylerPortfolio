a43d2c1a81e26c6e801d3b8cefc7c6af7b730fab
Tyler G <guthriet1230@gmail.com>
2018-10-25 23:07:16 -0400

    additions after looking at websites

---
+++ assets/javascript/javascript-portfolio.js
@@ -0,0 +1,54 @@
+////////////////////////////////////////
+// Hangman Animation
+////////////////////////////////////////
+
+$('div.hangman').mouseenter(function() {
+    $(this).css("cursor","pointer");
+    $('h2#hangman').animate({"top": "-=35px", "opacity": "1"}, "slow" );
+});
+
+$('div.hangman').mouseleave(function() {   
+    $('h2#hangman').animate({"top": "+=35px", "opacity": "0"}, "slow" );
+});
+
+////////////////////////////////////////
+// Unit-4-Game Animation
+////////////////////////////////////////
+
+$('div.unit-4-game').mouseenter(function() {
+    $(this).css("cursor","pointer");
+    $('h2#unit-4-game').animate({"top": "-=35px","opacity": "1"}, "slow" );
+});
+
+$('div.unit-4-game').mouseleave(function() {   
+    $('h2#unit-4-game').animate({"top": "+=35px", "opacity": "0"}, "slow" );
+});
+
+
+////////////////////////////////////////
+// Trivia Game Animation
+////////////////////////////////////////
+$('div.trivia-game').mouseenter(function() {
+    $(this).css("cursor","pointer");
+    $('h2#trivia-game').animate({"top": "-=35px", "opacity": "1" }, "slow");
+});
+
+$('div.trivia-game').mouseleave(function() {   
+    $('h2#trivia-game').animate({"top": "+=35px", "opacity": "0"}, "slow");
+});
+
+////////////////////////////////////////
+// Rock Paper Scissors Animation
+////////////////////////////////////////
+$('div.rock-paper-scissors').mouseenter(function() {
+    $(this).css("cursor","pointer");
+    $('h2#rock-paper-scissors').animate({"top": "-=35px", "opacity": "1" }, "slow");
+});
+
+$('div.rock-paper-scissors').mouseleave(function() {   
+    $('h2#rock-paper-scissors').animate({"top": "+=35px", "opacity": "0"}, "slow");
+});
+
+
+
+
