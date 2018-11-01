6fa3b9ff020f9ffb491e687629a94837614da17f
Tyler G <guthriet1230@gmail.com>
2018-11-01 00:54:04 -0500

    responsive fix

--- assets/javascript/javascript-portfolio.js
+++ assets/javascript/javascript-portfolio.js
@@ -1,14 +1,18 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                          ANIMATIONS
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 ////////////////////////////////////////
 // Hangman Animation
 ////////////////////////////////////////
 
 $('div.hangman').mouseenter(function() {
     $(this).css("cursor","pointer");
-    $('h2#hangman').animate({"top": "-=35px", "opacity": "1"}, "slow" );
+    $('h2#hangman').animate({"top": "-=15%", "opacity": "1"}, "slow" );
 });
 
 $('div.hangman').mouseleave(function() {   
-    $('h2#hangman').animate({"top": "+=35px", "opacity": "0"}, "slow" );
+    $('h2#hangman').animate({"top": "+=15%", "opacity": "0"}, "slow" );
 });
 
 ////////////////////////////////////////
@@ -17,11 +21,11 @@ $('div.hangman').mouseleave(function() {
 
 $('div.unit-4-game').mouseenter(function() {
     $(this).css("cursor","pointer");
-    $('h2#unit-4-game').animate({"top": "-=35px","opacity": "1"}, "slow" );
+    $('h2#unit-4-game').animate({"top": "-=15%","opacity": "1"}, "slow" );
 });
 
 $('div.unit-4-game').mouseleave(function() {   
-    $('h2#unit-4-game').animate({"top": "+=35px", "opacity": "0"}, "slow" );
+    $('h2#unit-4-game').animate({"top": "+=15%", "opacity": "0"}, "slow" );
 });
 
 
@@ -30,11 +34,11 @@ $('div.unit-4-game').mouseleave(function() {
 ////////////////////////////////////////
 $('div.trivia-game').mouseenter(function() {
     $(this).css("cursor","pointer");
-    $('h2#trivia-game').animate({"top": "-=35px", "opacity": "1" }, "slow");
+    $('h2#trivia-game').animate({"top": "-=15%", "opacity": "1" }, "slow");
 });
 
 $('div.trivia-game').mouseleave(function() {   
-    $('h2#trivia-game').animate({"top": "+=35px", "opacity": "0"}, "slow");
+    $('h2#trivia-game').animate({"top": "+=15%", "opacity": "0"}, "slow");
 });
 
 ////////////////////////////////////////
@@ -42,23 +46,49 @@ $('div.trivia-game').mouseleave(function() {
 ////////////////////////////////////////
 $('div.rock-paper-scissors').mouseenter(function() {
     $(this).css("cursor","pointer");
-    $('h2#rock-paper-scissors').animate({"top": "-=35px", "opacity": "1" }, "slow");
+    $('h2#rock-paper-scissors').animate({"top": "-=15%", "opacity": "1" }, "slow");
 });
 
 $('div.rock-paper-scissors').mouseleave(function() {   
-    $('h2#rock-paper-scissors').animate({"top": "+=35px", "opacity": "0"}, "slow");
+    $('h2#rock-paper-scissors').animate({"top": "+=15%", "opacity": "0"}, "slow");
+});
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                    Buttons - Display
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$(document).ready(function() {
+    // console.log( "ready!" );
+    $("#portfolio-artwork").hide();
+    $("#portfolio-professional").hide();
+    $("#portfolio-games").show();
 });
 
 
+
 $("#professional-button").on("click", function(){
-    console.log("hello")
-})
+    // console.log("hello");
+    $("#portfolio-professional").show();
+    $("#portfolio-games").hide();
+    $("#portfolio-artwork").hide();
+});
 
 $("#games-button").on("click", function(){
-    console.log("hello")
-})
+    // console.log("hello");
+    $("#portfolio-professional").hide();
+    $("#portfolio-games").show();
+    $("#portfolio-artwork").hide();
+  
+});
 
 
 $("#artwork-button").on("click", function(){
-    console.log("hello")
-})
+    // console.log("hello");
+    $("#portfolio-professional").hide();
+    $("#portfolio-games").hide();
+    $("#portfolio-artwork").show();
+   
+});
+
+
+
--- assets/javascript/javascript-resume.js
+++ assets/javascript/javascript-resume.js
@@ -1,31 +1,38 @@
+
+$(document).ready(function() {
+    // console.log( "ready!" );
+    $("#job-description-arc").hide();
+    $("#job-description-northwestern").hide();
+    $("#job-description-leo").show();
+});
+
 ////////////////////////////////////////////////////////////////////////
 // ARC ON CLICK
 ////////////////////////////////////////////////////////////////////////
-$("#leo-job").on("click", "img", function() {
+$("#leo-job").on("click", function() {
     // console.log("clicked");
-    // $("#job-description-arc").style.disply = "none";
-    // $("#job-description-northwestern").style.disply = "none"; 
-    $("#job-description-leo").style.color = "blue";    
+    $("#job-description-arc").hide();
+    $("#job-description-northwestern").hide();
+    $("#job-description-leo").show();
   
 });
 
 ////////////////////////////////////////////////////////////////////////
 // ARC ON CLICK
 ////////////////////////////////////////////////////////////////////////
-$("#arc-job").on("click", "img", function() {
+$("#arc-job").on("click", function() {
     console.log("clicked");
-    
-    $("#job-description-leo").style.disply = "none";  
-    $("##job-description-northwestern").style.disply = "none";
-    $("#job-description-arc").style.disply = "block";
+    $("#job-description-leo").hide();
+    $("#job-description-northwestern").hide();
+    $("#job-description-arc").show();
 });             
 
 ////////////////////////////////////////////////////////////////////////
 // NORTHWESTERN ON CLICK
 ////////////////////////////////////////////////////////////////////////
-$("#northwestern-job").on("click", "img", function() {
-    console.log("clicked");
-    $("#job-description-leo").style.disply = "none";
-    $("#job-description-arc").style.disply = "none";
-    $("#job-description-northwestern").style.disply = "block";
+$("#northwestern-job").on("click", function() {
+    // console.log("clicked");
+    $("#job-description-leo").hide();
+    $("#job-description-arc").hide();
+    $("#job-description-northwestern").show();
 });
