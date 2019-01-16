0c5d18db6f0f2457379bd36ca7324fe555b2a7d7
Tyler G <guthriet1230@gmail.com>
2019-01-15 19:20:04 -0600

    updated fonts and portfolio

--- assets/javascript/javascript-portfolio.js
+++ assets/javascript/javascript-portfolio.js
@@ -53,42 +53,68 @@ $('div.rock-paper-scissors').mouseleave(function() {
     $('h2#rock-paper-scissors').animate({"top": "+=15%", "opacity": "0"}, "slow");
 });
 
+
+////////////////////////////////////////
+// Budzz Animation
+////////////////////////////////////////
+$('div.budzz').mouseenter(function() {
+    $(this).css("cursor","pointer");
+    $('h2#budzz').animate({"top": "-=15%", "opacity": "1" }, "slow");
+});
+
+$('div.budzz').mouseleave(function() {   
+    $('h2#budzz').animate({"top": "+=15%", "opacity": "0"}, "slow");
+});
+
+
+////////////////////////////////////////
+// Cup of Sugar Animation
+////////////////////////////////////////
+$('div.cup-of-sugar').mouseenter(function() {
+    $(this).css("cursor","pointer");
+    $('h2#cup-of-sugar').animate({"top": "-=15%", "opacity": "1" }, "slow");
+});
+
+$('div.cup-of-sugar').mouseleave(function() {   
+    $('h2#cup-of-sugar').animate({"top": "+=15%", "opacity": "0"}, "slow");
+});
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                    Buttons - Display
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-$(document).ready(function() {
-    // console.log( "ready!" );
-    $("#portfolio-artwork").hide();
-    $("#portfolio-professional").hide();
-    $("#portfolio-games").show();
-});
+// $(document).ready(function() {
+//     // console.log( "ready!" );
+//     $("#portfolio-artwork").hide();
+//     $("#portfolio-professional").hide();
+//     $("#portfolio-games").show();
+// });
 
 
 
-$("#professional-button").on("click", function(){
-    // console.log("hello");
-    $("#portfolio-professional").show();
-    $("#portfolio-games").hide();
-    $("#portfolio-artwork").hide();
-});
+// $("#professional-button").on("click", function(){
+//     // console.log("hello");
+//     $("#portfolio-professional").show();
+//     $("#portfolio-games").hide();
+//     $("#portfolio-artwork").hide();
+// });
 
-$("#games-button").on("click", function(){
-    // console.log("hello");
-    $("#portfolio-professional").hide();
-    $("#portfolio-games").show();
-    $("#portfolio-artwork").hide();
+// $("#games-button").on("click", function(){
+//     // console.log("hello");
+//     $("#portfolio-professional").hide();
+//     $("#portfolio-games").show();
+//     $("#portfolio-artwork").hide();
   
-});
+// });
 
 
-$("#artwork-button").on("click", function(){
-    // console.log("hello");
-    $("#portfolio-professional").hide();
-    $("#portfolio-games").hide();
-    $("#portfolio-artwork").show();
+// $("#artwork-button").on("click", function(){
+//     // console.log("hello");
+//     $("#portfolio-professional").hide();
+//     $("#portfolio-games").hide();
+//     $("#portfolio-artwork").show();
    
-});
+// });
 
 
 
