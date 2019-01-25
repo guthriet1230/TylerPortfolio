6646bcb9ec8d0d2f6ac227d41b718dcfa6fa386c
Tyler G <guthriet1230@gmail.com>
2019-01-24 19:37:59 -0600

    added brand statement

--- assets/javascript/javascript-portfolio.js
+++ assets/javascript/javascript-portfolio.js
@@ -66,6 +66,18 @@ $('div.budzz').mouseleave(function() {
     $('h2#budzz').animate({"top": "+=15%", "opacity": "0"}, "slow");
 });
 
+////////////////////////////////////////
+// Friend Finder Animation
+////////////////////////////////////////
+$('div.friend-finder').mouseenter(function() {
+    $(this).css("cursor","pointer");
+    $('h2#friend-finder').animate({"top": "-=15%", "opacity": "1" }, "slow");
+});
+
+$('div.friend-finder').mouseleave(function() {   
+    $('h2#friend-finder').animate({"top": "+=15%", "opacity": "0"}, "slow");
+});
+
 
 ////////////////////////////////////////
 // Cup of Sugar Animation
@@ -78,6 +90,87 @@ $('div.cup-of-sugar').mouseenter(function() {
 $('div.cup-of-sugar').mouseleave(function() {   
     $('h2#cup-of-sugar').animate({"top": "+=15%", "opacity": "0"}, "slow");
 });
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                    Modals
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+// Get the modal
+var modalArc = document.getElementById('myModal-Arc');
+var modalLeo = document.getElementById('myModal-Leo');
+var modalNorthwestern = document.getElementById('myModal-Northwestern');
+var modalUCSB = document.getElementById('myModal-UCSB');
+var btnArc = document.getElementById("myBtn-Arc");
+var btnLeo = document.getElementById("myBtn-Leo");
+var btnNorthwestern = document.getElementById("myBtn-Northwestern");
+var btnUCSB = document.getElementById("myBtn-UCSB");
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modalArc.style.display = "block";
+}
+
+btn.onclick = function() {
+    modalLeo.style.display = "block";
+  }
+
+  btn.onclick = function() {
+    modalNorthwestern.style.display = "block";
+  }
+
+  btn.onclick = function() {
+    modalUCSB.style.display = "block";
+  }
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modalArc.style.display = "none";
+}
+
+span.onclick = function() {
+    modalLeo.style.display = "none";
+  }
+
+  span.onclick = function() {
+    modalNorthwestern.style.display = "none";
+  }
+
+  span.onclick = function() {
+    modalUCSB.style.display = "none";
+  }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modalArc) {
+    modalArc.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+    if (event.target == modalLeo) {
+      modalLeo.style.display = "none";
+    }
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modalNorthwestern) {
+      modalNorthwestern.style.display = "none";
+    }
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modalUCSB) {
+      modalUCSB.style.display = "none";
+    }
+  }
+
+
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //                                                    Buttons - Display
