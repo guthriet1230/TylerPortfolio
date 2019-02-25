cd5b15af8bb6275187bcfd39b5e398c9cd1a6f17
Tyler G <guthriet1230@gmail.com>
2019-02-24 23:51:20 -0600

    updates for applying

--- public/assets/javascript/javascript-portfolio.js
+++ public/assets/javascript/javascript-portfolio.js
@@ -3,45 +3,6 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 ////////////////////////////////////////
-// Hangman Animation
-////////////////////////////////////////
-
-$('div.hangman').mouseenter(function() {
-    $(this).css("cursor","pointer");
-    $('h2#hangman').animate({"top": "-=15%", "opacity": "1"}, "slow" );
-});
-
-$('div.hangman').mouseleave(function() {   
-    $('h2#hangman').animate({"top": "+=15%", "opacity": "0"}, "slow" );
-});
-
-////////////////////////////////////////
-// Unit-4-Game Animation
-////////////////////////////////////////
-
-$('div.unit-4-game').mouseenter(function() {
-    $(this).css("cursor","pointer");
-    $('h2#unit-4-game').animate({"top": "-=15%","opacity": "1"}, "slow" );
-});
-
-$('div.unit-4-game').mouseleave(function() {   
-    $('h2#unit-4-game').animate({"top": "+=15%", "opacity": "0"}, "slow" );
-});
-
-
-////////////////////////////////////////
-// Trivia Game Animation
-////////////////////////////////////////
-$('div.trivia-game').mouseenter(function() {
-    $(this).css("cursor","pointer");
-    $('h2#trivia-game').animate({"top": "-=15%", "opacity": "1" }, "slow");
-});
-
-$('div.trivia-game').mouseleave(function() {   
-    $('h2#trivia-game').animate({"top": "+=15%", "opacity": "0"}, "slow");
-});
-
-////////////////////////////////////////
 // Rock Paper Scissors Animation
 ////////////////////////////////////////
 $('div.rock-paper-scissors').mouseenter(function() {
@@ -53,6 +14,18 @@ $('div.rock-paper-scissors').mouseleave(function() {
     $('h2#rock-paper-scissors').animate({"top": "+=15%", "opacity": "0"}, "slow");
 });
 
+////////////////////////////////////////
+// Hangman Animation
+////////////////////////////////////////
+
+$('div.hangman').mouseenter(function() {
+    $(this).css("cursor","pointer");
+    $('h2#hangman').animate({"top": "-=15%", "opacity": "1"}, "slow" );
+});
+
+$('div.hangman').mouseleave(function() {   
+    $('h2#hangman').animate({"top": "+=15%", "opacity": "0"}, "slow" );
+});
 
 ////////////////////////////////////////
 // Budzz Animation
@@ -90,6 +63,19 @@ $('div.cup-of-sugar').mouseenter(function() {
 $('div.cup-of-sugar').mouseleave(function() {   
     $('h2#cup-of-sugar').animate({"top": "+=15%", "opacity": "0"}, "slow");
 });
+
+////////////////////////////////////////
+// News-Scraper Animation
+////////////////////////////////////////
+$('div.news-scraper').mouseenter(function() {
+    $(this).css("cursor","pointer");
+    $('h2#news-scraper').animate({"top": "-=15%", "opacity": "1" }, "slow");
+});
+
+$('div.news-scraper').mouseleave(function() {   
+    $('h2#news-scraper').animate({"top": "+=15%", "opacity": "0"}, "slow");
+});
+
 
 
 
